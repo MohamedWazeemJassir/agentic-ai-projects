5e0fd2e84818325a08d29323ffe7d9fdd7829302
wazeem <mohamedwazeemjassir@gmail.com>
2026-09-18 23:58:02 +0530

    create voice agent

---
+++ 13_voice_agent/main.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+def main():
+    r = sr.recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        r.pause_thrshold = 2
+
+        print("Speak Something...")
+        audio = r.listen(source)
+
+        print("Processing Audio... (STT)")
+        stt = r.recognize_google(audio)
+
+        print("You Said:", stt)
+
+main()
